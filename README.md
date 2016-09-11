cffddd1308ef9237808252a7b4ab55b98156cfa8
Aniruddha. A <ani@versa-networks.com>
2016-09-11 15:49:02 +0530

    Added dumper (indent + strip comment)
    Also adding pprint from https://github.com/jagt/pprint.lua
    For debug

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # LYang
 
-Lua (LPeg) based Yang recognizer and validator.
+Lua [LPeg](http://www.inf.puc-rio.br/~roberto/lpeg/) based [YANG](https://tools.ietf.org/html/rfc6020) recognizer and validator.
+
+# Required packages:
+
+    $ sudo apt-get install lua5.2 lua-lpeg
--- yang.lua
+++ yang.lua
@@ -1,8 +1,14 @@
 local lpeg = require 'lpeg'
+local pp   = require 'pprint'
 
-local exit=os.exit
+local exit  = os.exit
+local debug = true
 
-function jcat (acc, s)
+function trim(s) -- trim5 of lua-users
+    return s:match'^%s*(.*%S)' or ''
+end
+
+function jcat(acc, s)
     --[[
     local a = acc:gsub('"','')
     a = a:gsub("'", '')
@@ -12,6 +18,58 @@ function jcat (acc, s)
     ]]
     -- Lets sanitize str before use, not here
     return acc..s
+end
+
+function getnode(p)
+    return { kids={}, parent=p }
+end
+
+local tree    = getnode(nil)
+local cur     = tree
+local id, val = nil, nil
+function do_op(type,...)
+    local tok = {...}
+    local node
+
+    if debug then print(type,...) end
+
+    if type == 'ID' then
+        id  = trim(tok[1])
+        val = nil
+    elseif type == 'VAL' then
+        val = trim(tok[1])
+    elseif type == 'END' then
+        table.insert(cur.kids, { ['id']=id, ['val']=val, ['node']=nil } )
+        id, val = nil, nil
+    elseif type == 'OPEN' then
+        node = getnode(cur)
+        table.insert(cur.kids, { ['id']=id, ['val']=val, ['node']=node } )
+        cur           = node
+        id, val, node = nil, nil, nil
+    elseif type == 'CLOSE' then
+        cur = cur.parent
+    else
+        print 'Err'
+    end
+end
+
+function _indent(t, nsp)
+    local sp = ' '
+    if t.kids then
+        for _, k in ipairs(t.kids) do
+            if not k.node then
+                print(sp:rep(nsp).. k.id ..' '.. k.val ..';')
+            else
+                print(sp:rep(nsp).. k.id ..' '.. k.val ..' {')
+                _indent(k.node, nsp + 4)
+                print(sp:rep(nsp)..'}')
+            end
+        end
+    end
+end
+
+function indent_dump(t)
+    return _indent(t, 0)
 end
 
 function domatch (data)
@@ -29,17 +87,17 @@ function domatch (data)
     local bw         =  (alnum + S'/:*![]=')   --   at least some junk barewords!
     local identifier =  letter * alnum^0
     local ns_id      = _ * C((identifier * P':')^-1 * identifier) * chk
-                                / function(...) print('ID',...) end
+                                / function(...) do_op('ID',...) end
     local string     = _ * (P'"' * (P'\\' * P(1) + (1 - S'\\"'))^0 * P'"') +
                        _ * (P"'" * (P'\\' * P(1) + (1 - S"\\'"))^0 * P"'")
     local concat     = _ * P'+'
     local jstring    = (C(string) * concat)^0 * C(string)
     local value      = _ * (bw^1 + Cf(jstring,jcat))
-                                / function(...) print('VAL',...) end
+                                / function(...) do_op('VAL',...) end
     -- We dont need captures for the foll - remove after debug
-    local stmtend    = _ * C(P';') / function(...) print('END',...) end
-    local obrace     = _ * C(P'{') / function(...) print('OPEN',...) end
-    local cbrace     = _ * C(P'}') / function(...) print('CLOSE',...) end
+    local stmtend    = _ * C(P';') / function(...) do_op('END',...) end
+    local obrace     = _ * C(P'{') / function(...) do_op('OPEN',...) end
+    local cbrace     = _ * C(P'}') / function(...) do_op('CLOSE',...) end
 
     local yang = P({
         "block",
@@ -55,6 +113,10 @@ function domatch (data)
     else
         if n == #data then
             print("Matched entirely")
+            if debug then
+                pp.pprint(tree)
+                indent_dump(tree)
+            end
         end
     end
 end
